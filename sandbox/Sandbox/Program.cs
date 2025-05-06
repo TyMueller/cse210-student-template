f204d96d418beeafa325116c0e76636aa01379f0
Ty Miller <tyghvm19@byui.edu>
2025-05-05 18:53:06 -0600

    Made James Bond name program

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,10 +1,50 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
+        /*
+        int x;
+        x = 5;
+
+        int y = 33;
+
+
         Console.WriteLine("Hello Stinkey World!");
-        Console.WriteLine("Testing to main");
+
+        Console.WriteLine($"X is {x} and y is {y} so them added is {x + y}"); //How can I add these together?
+        */
+
+        /*Have a user enter their name:
+
+        Console.Write("Enter your name: ");
+        string name = Console.ReadLine();
+        Console.WriteLine("Your name is " + name);
+        */
+        /*
+        //Freezing temperature checker
+        int freezing = 32;
+
+        Console.Write("What is the temperature? ");
+        string current_temp_string = Console.ReadLine(); //Can only get strings returned
+        int current_temp_int = int.Parse(current_temp_string);  //Convert string to integer and rename data type
+
+        Console.WriteLine("It is " + current_temp_int + " degrees");
+        Console.WriteLine($"{current_temp_int + 10} add 10 test");
+        
+
+        if (current_temp_int < freezing - 32)
+        {
+            Console.WriteLine("It is very VERY cold!");
+        }
+        else if (current_temp_int <= freezing)
+        {
+            Console.WriteLine("You are probably pretty cold");
+        }
+        */
+
+
     }
 }
